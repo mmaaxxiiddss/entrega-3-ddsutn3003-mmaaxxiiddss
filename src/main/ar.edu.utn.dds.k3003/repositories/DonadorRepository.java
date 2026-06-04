a8d2bf970bdbf109e7cc91df7bb0f7d84af1a5d0
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-04 17:09:21 -0300

    Add fachada

--- src/main/ar.edu.utn.dds.k3003/repositories/DonadorRepository.java
+++ src/main/ar.edu.utn.dds.k3003/repositories/DonadorRepository.java
@@ -1,1 +1,16 @@
+package ar.edu.utn.dds.k3003.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
+public interface DonadorRepository{
+  
+  Optional<Donador> findById(String id);
+
+  Donador save(Donador donador);
+
+  Donador deleteById(String id);
+
+  Donador findAll();
+
+}
