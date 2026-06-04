0d589a20495583bfc2b676463cc7b17338d04294
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-04 17:22:22 -0300

    Add fachada

---
+++ src/main/ar.edu.utn.dds.k3003/repositories/InMemoryQuejaRepository.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.dds.k3003.repositories;
+
+import java.util.List;
+
+public class InMemoryQuejaRepository implements QuejasRepository{
+
+   @Override
+   Optional<Queja> findById(String id){
+       
+   }
+
+  
+}
