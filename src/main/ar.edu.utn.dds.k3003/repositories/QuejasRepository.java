1bd9051be2db8285cbd7147996982de65d67f9a6
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-04 17:10:53 -0300

    Add fachada

--- src/main/ar.edu.utn.dds.k3003/repositories/QuejasRepository.java
+++ src/main/ar.edu.utn.dds.k3003/repositories/QuejasRepository.java
@@ -1,12 +1,15 @@
 package ar.edu.utn.dds.k3003.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface QuejasRepository{
 
-   Queja findById(String id);
+   Optional<Queja> findById(String id);
 
    Queja save(Queja queja);
+
+   Queja deleteById(String id);
 
    List<Queja> findAll();
 
