f296eae53a1e8a2ac39634dd65bd6fbf9443fcba
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-06 17:50:15 -0300

    Add fachada

---
+++ src/main/ar.edu.utn.dds.k3003/ImplFachadaLogistica.java
@@ -0,0 +1,14 @@
+package ar.edu.utn.dds.k3003;
+
+import java.util.List;
+
+public class ImplFachadaLogistica{
+
+  @Override
+  void setAlgoritmoMM(String depositoID, TipoAlgoritmoEnum tipoAlgoritmo){
+
+
+    
+  }
+
+}
