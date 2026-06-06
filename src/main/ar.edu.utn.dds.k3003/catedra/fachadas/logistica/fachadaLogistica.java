9dbb90718999c1ec573fdfbacd182f808d9452ea
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-06 17:43:19 -0300

    Add fachada

--- src/main/ar.edu.utn.dds.k3003/catedra/fachadas/logistica/fachadaLogistica.java
+++ src/main/ar.edu.utn.dds.k3003/catedra/fachadas/logistica/fachadaLogistica.java
@@ -1,1 +1,33 @@
+package ar.edu.utn.dds.k3003.catedra.fachadas;
 
+
+import ar.edu.utn.dds.k3003.catedra.dtos.donadoresYEntidades.NecesidadMaterialDTO;
+import ar.edu.utn.dds.k3003.catedra.dtos.logistica.*;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+
+public interface FachadaLogistica {
+
+  DepositoDTO agregarDeposito(DepositoDTO deposito);
+
+  DepositoDTO buscarDepositoPorID(String depositoID) throws NoSuchElementException;
+
+  AsignacionDTO buscarAsignacionPorPaqueteID(String paqueteID) throws NoSuchElementException;
+
+  DepositoDTO gestionarDonacion(
+      String depositoID, String donacionID, String productoID, Integer cantidad)
+      throws NoSuchElementException;
+
+  void setAlgoritmoMM(String depositoID, TipoAlgoritmoEnum tipoAlgoritmo);
+
+  AsignacionDTO ejecutarMatchmaking(
+      String depositoID, PaqueteDTO paqueteDTO, List<NecesidadMaterialDTO> necesidades);
+
+  void reportarEntrega(PaqueteDTO paqueteDTO);
+
+  void setFachadaDonadoresYEntidades(FachadaDonadoresYEntidades fachadaDonadoresYEntidades);
+
+  void setFachadaDonaciones(FachadaDonaciones fachadaDonaciones);
+ 
+}
