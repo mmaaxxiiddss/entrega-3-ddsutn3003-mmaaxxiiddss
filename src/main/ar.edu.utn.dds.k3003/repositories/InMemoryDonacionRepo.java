617d7a5927f29fa10432f4a4e4a06aeac58ea8ff
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-07 11:38:42 -0300

    Add fachada

---
+++ src/main/ar.edu.utn.dds.k3003/repositories/InMemoryDonacionRepo.java
@@ -0,0 +1,39 @@
+package ar.edu.utn.dds.k3003.repositories;
+
+
+import java.util.List;
+
+
+public class InMemoryDonacionRepo implements DonacionRepository{
+
+  private List<Donacion> donaciones;
+  private AtomicLong idSecuencial = new AtomicLong(1);
+
+  public InMemoryDonacionRepo() {
+    this.donaciones = new ArrayList<>();
+  }
+
+   
+   @Override
+   Optional<Donacion> findById(String id){
+       return this.donaciones.stream().filter(q -> q.getId().equals(id)).findFirst();
+   }
+
+
+  @Override
+  Donacion save(Donacion donacion)
+  {
+    Donacion donacionConID = donacion;
+    donacionConID.setId(String.valueOf(idSecuencial.getAndIncrement()));
+
+    this.donaciones.add(donacionConID);
+    return this.findById(donacionConID.getId()).get();
+  }
+
+  @Override
+  List<Queja> findAll()
+  {
+      return this.donaciones;
+  }
+  
+}
