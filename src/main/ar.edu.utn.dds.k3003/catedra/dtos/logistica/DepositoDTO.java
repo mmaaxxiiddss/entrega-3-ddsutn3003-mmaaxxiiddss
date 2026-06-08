0f7394e69dd1ac81d63b86c841e94e8d3e762afb
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-08 08:04:07 -0300

    Add fachada

---
+++ src/main/ar.edu.utn.dds.k3003/catedra/dtos/logistica/DepositoDTO.java
@@ -0,0 +1,11 @@
+package ar.edu.utn.dds.k3003.catedra.dtos.logistica;
+
+import java.util.List;
+
+public record DepositoDTO(
+    String id,
+    TipoAlgoritmoEnum algoritmo,
+    String nombre,
+    String direccion,
+    Integer capacidadMaxima,
+    List<PaqueteDTO> stockActual) {}
