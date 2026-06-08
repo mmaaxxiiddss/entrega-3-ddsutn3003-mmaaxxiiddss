d8eeb407d8f9e5c74867c573c9546e0dec94f361
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-08 08:19:24 -0300

    Add fachada

--- src/main/ar.edu.utn.dds.k3003/ImplFachadaLogistica.java
+++ src/main/ar.edu.utn.dds.k3003/ImplFachadaLogistica.java
@@ -6,35 +6,48 @@ import java.util.List;
 
 public class ImplFachadaLogistica{
 
+  private AsignacionRepository asignacionRepo;
+  private AsignacionDataMapper asignacionDataMapper;
+  private DepositoRepository depositoRepo;
+  private DepositoDataMapper depositoDataMapper;
+
+  public class ImplFachadaLogistica(){
+       super();
+       this.depositoRepo = new InMemoryDepositoRepo();
+  }
   
   @Override
   void setAlgoritmoMM(String depositoID, TipoAlgoritmoEnum tipoAlgoritmo){
 
       DepositoDTO depositoDTO = buscarDepositoPorID(depositoID);
+      depositoDTO.setAlgoritmo(tipoAlgoritmo);
       
-    
   }
 
   @Override
   AsignacionDTO ejecutarMatchmaking(
       String depositoID, PaqueteDTO paqueteDTO, List<NecesidadMaterialDTO> necesidades){
+
       
   }
   
   @Override
   void reportarEntrega(PaqueteDTO paqueteDTO){
-       
+      
+        
   }
 
   @Override
   void setFachadaDonadoresYEntidades(FachadaDonadoresYEntidades fachadaDonadoresYEntidades){
 
+
+      
   }
   
   @Override
   void setFachadaDonaciones(FachadaDonaciones fachadaDonaciones){
 
-
+      
   }
   
   
