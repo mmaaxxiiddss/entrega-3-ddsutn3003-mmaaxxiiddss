0a91315ffcfc8abb3c7ebfb65658a2058714421e
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-11 19:33:54 -0300

    Add fachada

---
+++ src/main/ar.edu.utn.dds.k3003/catedra/model/NecesidadMaterial.java
@@ -0,0 +1,18 @@
+package ar.edu.utn.dds.k3003;
+
+
+import java.util.List;
+
+
+public class NecesidadMaterial{
+
+    private String ID;
+  private String entidadID;
+  private int nivelDeUrgencia;
+  private String descripcion;
+  private int cantidadObjetivo;
+  private String productoSolicitadoID;
+  private TipoNecesidadMaterialEnum tipo;
+
+  
+}
