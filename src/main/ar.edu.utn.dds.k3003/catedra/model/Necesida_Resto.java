c4b691c2882d7758c046be2732f193ef17a7bf76
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-10 23:15:22 -0300

    Add fachada

---
+++ src/main/ar.edu.utn.dds.k3003/catedra/model/Necesida_Resto.java
@@ -0,0 +1,16 @@
+package ar.edu.utn.dds.k3003;
+
+
+import java.util.List;
+
+
+public class Necesidad_Clase(){
+
+    private String necesidadID;
+    private String resto;
+
+    public String getNecesidadID()
+      {
+           return this.necesidadID;
+      }
+}
