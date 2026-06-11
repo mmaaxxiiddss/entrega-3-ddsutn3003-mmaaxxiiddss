99d7da2a0cd3996c15ed5672afc4d52c92bcafac
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-11 19:27:56 -0300

    Add fachada

---
+++ src/main/ar.edu.utn.dds.k3003/catedra/model/EntidadBenefica.java
@@ -0,0 +1,11 @@
+package ar.edu.utn.dds.k3003.catedra.model;
+
+public class EntidadBeneficaDTO{
+  
+   private String id;
+ private String razonSocial;
+ private String domicilio;
+ private String telefono;
+ private String correo;
+  
+  }
