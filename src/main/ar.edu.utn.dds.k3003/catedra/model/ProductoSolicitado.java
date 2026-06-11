017ebbc958339215008320a1d26d5dc10159721a
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-11 19:37:48 -0300

    Add fachada

---
+++ src/main/ar.edu.utn.dds.k3003/catedra/model/ProductoSolicitado.java
@@ -0,0 +1,16 @@
+package ar.edu.utn.dds.k3003.catedra.model;
+
+
+import java.util.List;
+
+
+public class ProductoSolicitado{
+
+    private String ID;
+  private String nombre;
+  private String descripcion;
+
+
+
+
+}
