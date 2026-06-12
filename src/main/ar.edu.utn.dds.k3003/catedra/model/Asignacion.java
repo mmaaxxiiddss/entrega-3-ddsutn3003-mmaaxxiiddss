4241fe2065f6c055310bd132f9aeeb7ba3163259
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-12 07:52:26 -0300

    Create Asignacion class with initial fields

---
+++ src/main/ar.edu.utn.dds.k3003/catedra/model/Asignacion.java
@@ -0,0 +1,15 @@
+package ar.edu.utn.dds.k3003.catedra.model;
+
+
+import java.time.LocalDate;
+
+
+public class Asignacion{
+  
+    private String ID;
+    private String paqueteID;
+    private String necesidadID;
+    private LocalDateTime fecha;
+    private EstadoAsginacionEnum estado;
+  
+  }
