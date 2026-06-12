e5d59f7447f167bcb76ef4704774c12eb019567b
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-12 07:56:08 -0300

    Refactor DonadorStats class to use private fields

---
+++ src/main/ar.edu.utn.dds.k3003/catedra/model/DonadorStats.java
@@ -0,0 +1,18 @@
+package ar.edu.utn.dds.k3003.catedra.model;
+
+
+import java.util.List;
+
+
+public class DonadorStats(
+
+    private String ID;
+    private String nombre;
+    private String apellido;
+    private Integer edad;
+    private EstadoDonadorEnum estado;
+    private String categoria;
+    private String misionActualID;
+    private List<String> insigniasID;
+
+}
