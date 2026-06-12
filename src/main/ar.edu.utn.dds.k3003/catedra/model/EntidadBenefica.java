4fa4cec0a4d506931e561bb3b08417a44939abc0
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-12 07:47:14 -0300

    Refactor EntidadBeneficaDTO to EntidadBenefica

--- src/main/ar.edu.utn.dds.k3003/catedra/model/EntidadBenefica.java
+++ src/main/ar.edu.utn.dds.k3003/catedra/model/EntidadBenefica.java
@@ -1,11 +1,13 @@
 package ar.edu.utn.dds.k3003.catedra.model;
 
-public class EntidadBeneficaDTO{
+
+public class EntidadBenefica{
   
-   private String id;
+ private String id;
  private String razonSocial;
  private String domicilio;
  private String telefono;
  private String correo;
+
   
-  }
+}
