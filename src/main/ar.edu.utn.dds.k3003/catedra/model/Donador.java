3ee202fd843ce729b384364d4eefb8c77bcc6207
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-06-12 07:35:05 -0300

    Create Donador class with donor attributes

---
+++ src/main/ar.edu.utn.dds.k3003/catedra/model/Donador.java
@@ -0,0 +1,16 @@
+package ar.edu.utn.dds.k3003.catedra.model;
+
+
+public class Donador{
+  
+    private String id;
+    private String nombre;
+    private String apellido;
+    private Integer edad;
+    private String email;
+    private String nroDocumento;
+    private String domicilio;
+    private EstadoDonadorEnum estado;
+    private String categoria;
+  
+}
